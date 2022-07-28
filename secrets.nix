144dec135bfd46052ca82fc2046a7825e6171aeb
averyanalex <alex@averyan.ru>
2022-07-28 23:23:41 +0300

    Agenix secrets.nix

---
+++ secrets.nix
@@ -0,0 +1,10 @@
+let
+  alex = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINF5KDy1T6Z+RlDb+Io3g1uSZ46rhBxhNE39YlG3GPFM";
+  cofob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx";
+  users = [ alex cofob ];
+
+  systems = [ ];
+in
+{
+  # "secrets/passwords/alex.age".publicKeys = users ++ systems;
+}

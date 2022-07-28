82b9796397a0397438fbd97d71ee736609629f17
averyanalex <alex@averyan.ru>
2022-07-29 00:12:59 +0300

    More common options

--- common.nix
+++ common.nix
@@ -9,7 +9,50 @@
     passwordAuthentication = false;
   };
 
-  system.stateVersion = "22.05";
+  boot = {
+    cleanTmpDir = true;
+    tmpOnTmpfs = true;
+  };
+
+  services.fstrim.enable = true;
+
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  security.sudo.wheelNeedsPassword = false;
 
   users.mutableUsers = false;
+
+  time.timeZone = "Europe/Moscow";
+
+  system = {
+    stateVersion = "22.05";
+    autoUpgrade = {
+      enable = true;
+      allowReboot = false;
+      flake = "github:fire-square/nixos";
+      dates = "4:45";
+    };
+  };
+
+  nix = {
+    useSandbox = true;
+    autoOptimiseStore = true;
+    readOnlyStore = true;
+    allowedUsers = [ "@wheel" "@users" ];
+    trustedUsers = [ "@wheel" ];
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      keep-outputs = true
+      keep-derivations = true
+    '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d --max-freed $((64 * 1024**3))";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "weekly" ];
+    };
+  };
 }

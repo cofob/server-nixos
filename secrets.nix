7aeb2fb6466278ba2a832b80ffd5678fe267777e
cofob <cofob@riseup.net>
2022-07-29 00:42:24 -0700

    Add "cofob" password

--- secrets.nix
+++ secrets.nix
@@ -3,7 +3,8 @@ let
   cofob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx";
   users = [ alex cofob ];
 
-  systems = [ ];
+  diamond = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJPimxmmhXzwCUaWVaB89R6HGJLPP5+EW76h7Qx2WnFY";
+  systems = [ diamond ];
 in
 {
   "secrets/passwords/alex.age".publicKeys = [ alex ] ++ systems;

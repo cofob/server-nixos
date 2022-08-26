26d93cab4cb5c906f5a706742ea2a17cd1044be8
cofob <cofob@riseup.net>
2022-08-26 21:22:40 +0700

    Rekey

--- secrets.nix
+++ secrets.nix
@@ -19,7 +19,7 @@ in
   "secrets/nebula/rat-crt.age".publicKeys = users ++ systems;
   "secrets/nebula/rat-key.age".publicKeys = users ++ [ rat ];
 
-  "secrets/credentials/cloudflare.age".publicKeys = users ++ [ eagle ];
+  "secrets/credentials/cloudflare.age".publicKeys = users ++ [ eagle rat ];
   "secrets/credentials/bps.age".publicKeys = users ++ [ rat ];
   "secrets/credentials/tgcaptcha.age".publicKeys = users ++ [ rat ];
 }

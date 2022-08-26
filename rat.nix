02f0c162fa068fbf76a41ed919e43b398c58d751
cofob <cofob@riseup.net>
2022-08-27 00:22:01 +0700

    Remove config

--- rat.nix
+++ rat.nix
@@ -43,13 +43,6 @@
     package = pkgs.unstable.gitea;
     lfs.enable = true;
     dump.enable = true;
-    settings.session.COOKIE_SECURE = true;
-    settings.database.DB_TYPE = "mysql";
-    settings.database.HOST = "127.0.0.1";
-    settings.database.PORT = "3301";
-    settings.database.NAME = "gitea";
-    settings.database.USER = "gitea";
-    settings.database.PASSWD = "#dbpass#";
     domain = "git.frsqr.xyz";
     rootUrl = "https://git.frsqr.xyz/";
     appName = "gitea: Smth here";

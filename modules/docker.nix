b66d17d006aa798f36be10cc265f63f23288db54
Egor Ternovoy <cofob@riseup.net>
2022-11-22 21:57:01 +0700

    Add watchtower

--- modules/docker.nix
+++ modules/docker.nix
@@ -5,4 +5,10 @@
     enable = true;
     autoPrune.enable = true;
   };
+
+  virtualisation.oci-containers.containers.wathtower = {
+    image = "containrrr/watchtower";
+    cmd = [ "--cleanup" "--interval" "600" ];
+    autoStart = true;
+  };
 }

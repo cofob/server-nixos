e425cf9412044adb9e73c092f9379e5ce056d26b
cofob <cofob@riseup.net>
2022-12-15 18:26:53 +0700

    Add trusted-public-keys

--- modules/common.nix
+++ modules/common.nix
@@ -43,6 +43,10 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
       ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+      ];
     };
 
     daemonCPUSchedPolicy = "batch";

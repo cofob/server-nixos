d19ce5d7ba10661e25c029bc62a721f127407795
cofob <cofob@riseup.net>
2022-12-15 17:07:01 +0700

    Fix

--- hardware/diamond.nix
+++ hardware/diamond.nix
@@ -31,6 +31,6 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

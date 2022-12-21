dd533615a621d447b68ec8c3955bbc4991e85fc0
cofob <cofob@riseup.net>
2022-12-21 13:51:20 +0700

    Remove custom proxy headers

--- machines/rat/services/proxy.nix
+++ machines/rat/services/proxy.nix
@@ -4,10 +4,6 @@
   services.fs-nginx.virtualHosts."cloud.frsqr.xyz" = {
     useACMEHost = "cloud.frsqr.xyz";
     locations."/".proxyPass = "http://10.100.0.2/";
-    locations."/".extraConfig = ''
-      proxy_set_header Host cloud.frsqr.xyz;
-      proxy_set_header X-Real-IP $remote_addr;
-    '';
   };
 
   security.acme.certs."cloud.frsqr.xyz" = { };

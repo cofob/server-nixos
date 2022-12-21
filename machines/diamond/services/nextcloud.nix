dbc39d887cd312e8e9c243f4475341c1095c2216
cofob <cofob@riseup.net>
2022-12-21 14:00:22 +0700

    Add redis caching for nextcloud

--- machines/diamond/services/nextcloud.nix
+++ machines/diamond/services/nextcloud.nix
@@ -12,6 +12,7 @@
     hostName = "cloud.frsqr.xyz";
     autoUpdateApps.enable = true;
     enableBrokenCiphersForSSE = false;
+    caching.redis = true;
     config = {
       dbtype = "pgsql";
       dbport = "5432";
@@ -23,6 +24,20 @@
       overwriteProtocol = "https";
       extraTrustedDomains = [ "10.100.0.2" ];
     };
+    extraOptions = {
+      redis = {
+        host = "/run/redis-nextcloud/redis.sock";
+        port = 0;
+        dbindex = 0;
+        password = "secret";
+        timeout = 1.5;
+      };
+    };
+  };
+
+  services.redis.servers."nextcloud" = {
+    enable = true;
+    requirePass = "secret";
   };
 
   services.backup.timers.daily = [

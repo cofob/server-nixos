d7cc8f3a0430c3b60a224b6c78bd4057dfaea711
cofob <cofob@riseup.net>
2022-12-21 13:29:20 +0700

    Add slash

--- machines/rat/services/proxy.nix
+++ machines/rat/services/proxy.nix
@@ -3,7 +3,7 @@
 {
   services.fs-nginx.virtualHosts."cloud.frsqr.xyz" = {
     useACMEHost = "cloud.frsqr.xyz";
-    locations."/".proxyPass = "http://10.100.0.2";
+    locations."/".proxyPass = "http://10.100.0.2/";
     locations."/".extraConfig = ''
       proxy_set_header Host cloud.frsqr.xyz;
       proxy_set_header X-Real-IP $remote_addr;

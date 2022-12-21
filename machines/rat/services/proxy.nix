4febe7aaca8e9b54290ab2ce6a6bdb799a6e522f
cofob <cofob@riseup.net>
2022-12-21 13:20:18 +0700

    Add proxy params

--- machines/rat/services/proxy.nix
+++ machines/rat/services/proxy.nix
@@ -4,6 +4,11 @@
   services.fs-nginx.virtualHosts."cloud.frsqr.xyz" = {
     useACMEHost = "cloud.frsqr.xyz";
     locations."/".proxyPass = "http://10.100.0.2";
+    locations."/".extraConfig = ''
+      proxy_set_header Host cloud.frsqr.xyz;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    '';
   };
 
   security.acme.certs."cloud.frsqr.xyz" = { };

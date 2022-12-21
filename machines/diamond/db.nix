3f2301c206d07904c54c1b028b5289fbe6fb11f7
cofob <cofob@riseup.net>
2022-12-21 13:06:44 +0700

    Upd

--- machines/diamond/db.nix
+++ machines/diamond/db.nix
@@ -18,6 +18,8 @@
       # TYPE  DATABASE        USER            ADDRESS                 METHOD
       local   all             all                                     trust
       host    all             all             127.0.0.1/32            trust
+      host    all             all             ::1/128                 trust
+      host    all             all             localhost               trust
     '';
   };
 

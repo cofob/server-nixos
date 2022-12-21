be76a365095a970bd0f2640ed3af5fd8368bbcb9
cofob <cofob@riseup.net>
2022-12-21 14:04:23 +0700

    Change max body size

--- machines/rat/services/proxy.nix
+++ machines/rat/services/proxy.nix
@@ -4,6 +4,9 @@
   services.fs-nginx.virtualHosts."cloud.frsqr.xyz" = {
     useACMEHost = "cloud.frsqr.xyz";
     locations."/".proxyPass = "http://10.100.0.2/";
+    locations."/".extraConfig = ''
+      client_max_body_size 512M;
+    '';
   };
 
   security.acme.certs."cloud.frsqr.xyz" = { };

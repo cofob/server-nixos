b4176ec955647e7924335e435685841df6cff00c
cofob <cofob@riseup.net>
2023-01-12 11:20:41 +0700

    Add new nix binary cache

--- modules/common.nix
+++ modules/common.nix
@@ -42,10 +42,12 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://cofob.cachix.org"
       ];
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cofob.cachix.org-1:pLP85fVQ2T+bbaggvq03aDdXbQWjY36Gkch14N8mus4="
       ];
     };
 

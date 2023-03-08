08a52fb2d324122fc89c5b098ef62fdc77d22fa9
cofob <cofob@riseup.net>
2023-03-08 23:33:27 +0700

    LE certs

--- machines/rat/services/bw.nix
+++ machines/rat/services/bw.nix
@@ -16,10 +16,8 @@
   security.acme.certs."bw.cofob.dev" = { };
 
   services.fs-nginx.virtualHosts."bw.cofob.dev" = {
-    onlyCloudflare = true;
+    useACMEHost = "bw.cofob.dev";
     locations."/".proxyPass = "http://127.0.0.1:8222/";
-    sslCertificate = config.age.secrets.cf-certs-cofob-dev-cert.path;
-    sslCertificateKey = config.age.secrets.cf-certs-cofob-dev-key.path;
   };
 
   services.backup.timers.daily = [
--- modules/acme.nix
+++ modules/acme.nix
@@ -2,15 +2,11 @@
 
 {
   config = lib.mkIf (config.security.acme.certs != { }) {
-    age.secrets.credentials-cloudflare.file = ../secrets/credentials/cloudflare.age;
-
     security.acme = {
       acceptTerms = true;
       defaults = {
-        email = "admin@firesquare.ru";
-        dnsResolver = "1.1.1.1:53";
-        dnsProvider = "cloudflare";
-        credentialsFile = config.age.secrets.credentials-cloudflare.path;
+        webroot = "/var/lib/acme/acme-challenge";
+        email = "i.am@cofob.dev";
       };
     };
   };

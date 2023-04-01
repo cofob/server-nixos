ab7c5515638866cc143f165793dc4c0d1d8afda5
cofob <cofob@riseup.net>
2023-04-01 12:09:57 +0700

    Revert "LE certs"
    This reverts commit 08a52fb2d324122fc89c5b098ef62fdc77d22fa9.

--- machines/rat/services/bw.nix
+++ machines/rat/services/bw.nix
@@ -16,8 +16,10 @@
   security.acme.certs."bw.cofob.dev" = { };
 
   services.fs-nginx.virtualHosts."bw.cofob.dev" = {
-    useACMEHost = "bw.cofob.dev";
+    onlyCloudflare = true;
     locations."/".proxyPass = "http://127.0.0.1:8222/";
+    sslCertificate = config.age.secrets.cf-certs-cofob-dev-cert.path;
+    sslCertificateKey = config.age.secrets.cf-certs-cofob-dev-key.path;
   };
 
   services.backup.timers.daily = [

c2cfb125bd345da7ab1307b91206bbf8a5a3d51f
cofob <cofob@riseup.net>
2023-04-01 12:09:16 +0700

    Revert "Remove ipfs gateway"
    This reverts commit 5e262474b9261010831bc1663563d3896934ed14.

--- configs/stateless-sites.nix
+++ configs/stateless-sites.nix
@@ -8,11 +8,22 @@
   age.secrets.cf-certs-cofob-dev-cert.owner = "nginx";
   age.secrets.cf-certs-cofob-dev-key.owner = "nginx";
 
+  age.secrets.credentials-ipfs-proxy.file = ../secrets/credentials/ipfs-proxy.age;
+  services.ipfs-proxy = {
+    enable = true;
+    envFile = config.age.secrets.credentials-ipfs-proxy.path;
+  };
+
   services.cofob-ru.enable = true;
   security.acme.certs."cofob.dev" = { };
+  security.acme.certs."ipfs.cofob.dev" = { };
 
   services.fs-nginx = {
     enable = lib.mkDefault true;
+    virtualHosts."ipfs.cofob.dev" = {
+      useACMEHost = "ipfs.cofob.dev";
+      locations."/".proxyPass = "http://127.0.0.1:2314/";
+    };
     virtualHosts."cofob.dev" = {
       useACMEHost = "cofob.dev";
       locations."/".proxyPass = "http://127.0.0.1:3000/";

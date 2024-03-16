6493aec688fbbaa389d77d702afbff5b93084033
cofob <cofob@riseup.net>
2024-03-16 23:41:28 +0400

    Disable third-party integrations in element web

--- machines/incognet/services/web.nix
+++ machines/incognet/services/web.nix
@@ -93,7 +93,14 @@
           default_server_config = {
             "m.homeserver".base_url = "https://matrix.cofob.dev";
             "m.homeserver".server_name = "cofob.dev";
+            "m.identity_server".base_url = "https://disabled.cofob.dev";
           };
+          integrations_ui_url = "https://disabled.cofob.dev/";
+          integrations_rest_url = "https://disabled.cofob.dev/";
+          integrations_widgets_urls = [ "https://disabled.cofob.dev/" ];
+          jitsi.preferred_domain = "disabled.cofob.dev";
+          element_call.url = "https://disabled.cofob.dev";
+          map_style_url = "https://disabled.cofob.dev";
         };
       };
     };

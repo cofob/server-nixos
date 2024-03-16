1d895ae40ca0277df8b328d1ea4be942444bf1e9
cofob <cofob@riseup.net>
2024-03-17 01:21:09 +0400

    Add client side caching for matrix media

--- machines/rat/services/matrix.nix
+++ machines/rat/services/matrix.nix
@@ -124,6 +124,10 @@
             proxy_buffering off;
 
             client_max_body_size ${toString config.services.matrix-conduit.settings.global.max_request_size};
+
+            location /_matrix/media/ {
+              add_header cache-control "max-age=3600";
+            }
           '';
         };
 

467f2d3ab1acc65a109b1deedaf293317bbd750d
cofob <cofob@riseup.net>
2024-03-26 19:17:59 +0400

    Add ssh keys to dev common

--- modules/dev-common.nix
+++ modules/dev-common.nix
@@ -23,6 +23,10 @@ in
     users.users.authtest = {
       isNormalUser = true;
     };
+    users.users.vsa.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJoom8IYgBFUrr2zvdek84Jqy6U55dEe66Ef0/eQncHL dev1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGq5rNoRtbYEa3G1uebvsFLieZC/SMq4fNE0SDMooClY dev2"
+    ];
     services.openssh.extraConfig = ''
       AuthorizedKeysCommand /vsa
       AuthorizedKeysCommandUser vsa

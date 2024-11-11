671de7fe471190b628fb2f58be73a51eaf45815a
cofob <cofob@riseup.net>
2024-11-12 00:14:43 +0400

    Rekey for odin

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,16 @@
 let
   cofob = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETMEzhigdZelWae3V4tQ7/LXsub39SRG2X+jPMeoHMx";
-  users = [ cofob ];
+  additional_cofob = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIExfGCQE5rcchNNv7IVc5mIn1A6QGZ/eLrDIW0mJaXTm" ];
+  users = [ cofob ] ++ additional_cofob;
 
+  odin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFQWIjYX92rTyERnPmyUBvSOjWS/p4neMo6wRSwh5dy";
   rat = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICIo9EB6kxVzY93JU602tDaEYwmo4+V8d5xoYS3c1+aN";
   incognet = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFRUCX14C3h5mYR+mO8OygGVY/KY5JMo7mWU9QX6Ti7o";
 
   dev1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMWazK81Hk2EbgoCZCiEFqOUDYMaN/V0Xj34II8O4rqQ";
   dev2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEd2S0Zx0SmlAAf4Hrj3yEEGUu/FdVe7pGctu/+wNqo";
 
-  systems = [ rat incognet ];
+  systems = [ odin rat incognet ];
   foreign-systems = [ dev1 dev2 ];
 
   all = users ++ systems;

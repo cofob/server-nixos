8e6f204518e2f79652a270873cd3c16bb129898e
cofob <cofob@riseup.net>
2024-12-13 22:28:54 +0400

    Open SSH port

--- machines/rat/ssh.nix
+++ machines/rat/ssh.nix
@@ -4,5 +4,5 @@
   # Allow to SSH into the machine over ZeroTier network
   networking.firewall.interfaces.ztwfuehebm.allowedTCPPorts = [ 22 ];
   # Close SSH port
-  services.openssh.openFirewall = false;
+  services.openssh.openFirewall = true;
 }
